abcfc2e1e0ce10aa133e197ea0c01c38050cfdcb
Colin McNally <colin@colinmcnally.ca>
2019-08-19 14:58:19 -0400

    add a script to extend the tmax in the .h5 files for the 9,4 runs by 1e9 kepler times

---
+++ chainbuilder/extendruns_nchain9_p4.py
@@ -0,0 +1,28 @@
+# ingest the orbits*.h5 files to a single HDF5 file library/database
+# 
+
+import numpy as np
+import sys
+import os
+import os.path
+import h5py
+
+import chainmodels
+
+allmod =  {**chainmodels.runs_ext1 , **chainmodels.runs_ext2}
+allfiles =  {**chainmodels.filenames_ext1 , **chainmodels.filenames_ext2}
+for key in allmod :
+  print(key,' ',allmod[key])
+
+for key in chainmodels.runs :
+  if chainmodels.runs[key][0] == 9 and chainmodels.runs[key][1] == 4:
+    allmod[key] = chainmodels.runs[key]
+    allfiles[key] = chainmodels.filenames[key]
+    print(key,' ',allmod[key])
+
+for key in allmod:
+  print(key,' ',allmod[key])
+  orb = h5py.File(allfiles[key], 'r+')
+
+  orb.attrs['tmax'] += 1e9*chainmodels.tkepler
+  orb.close()
