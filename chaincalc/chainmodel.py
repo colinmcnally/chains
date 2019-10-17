dde7cf2511b3e69e3e4ac845c126b00cfea6b636
Colin McNally <colin@colinmcnally.ca>
2019-10-17 18:07:20 +0100

    chainmodel class now runs and has archive and status outputs, and hooks for manual and collision stops from driver

--- chaincalc/chainmodel.py
+++ chaincalc/chainmodel.py
@@ -15,10 +15,15 @@
 #  class which figure out how to get all the models to be completed
 import sys
 import hashlib
+import time
+import random
+import json
 import numpy as np
+import rebound
+import reboundx
 
-class model:
-    def __init__(self, params):
+class Model:
+    def __init__(self, params, wall_start, snap_wall_interval=60*60):
         """ Params is a dict of parameters, easier to reuse when initializing in a loop
             aspectratio0
             sigma0
@@ -32,6 +37,7 @@ class model:
             pmass
             nchain
             p_res
+            a0
 
             some other values set as object attributes, but not intended to be modified
             G
@@ -39,7 +45,6 @@ class model:
             integrator
             integrator_dt
      """
-
         paramlist = ['aspectratio0',
                      'sigma0',
                      'redge',
@@ -51,7 +56,9 @@ class model:
                      'deltatdep',
                      'pmass',
                      'nchain',
-                     'p_res']
+                     'p_res',
+                     'q_res',
+                     'a0']
                      
         for parstr in paramlist:
             try:
@@ -69,10 +76,17 @@ class model:
         # 1e-2 times the inner edge orbital time
         self.integrator_dt = 1e-2*2.0*np.pi* self.redge**1.5
         self.set_model_hash()
+        self.simarchive_filename = 'sim'+self.hash+'.rbsa'
+        self.status_filename = 'sim'+self.hash+'_status.json'
+
+        self.wall_start = wall_start
+        self.lastheart = wall_start
+        self.heart_print_interval = 5.0
+        self.snap_wall_interval = snap_wall_interval
+
 
     def set_model_hash(self):
-        """Calculate a hash specifying the model. Need to use all the parameters of the model here
-        """
+        """Calculate a hash specifying the model. Need to use all the parameters of the model here."""
         params = self.params
         repstr = ''
         for pk in sorted(params.keys()):
@@ -83,3 +97,89 @@ class model:
         repstr += '{:e}'.format(self.integrator_dt)
         #calculate the MD5 hash of this string
         self.hash = hashlib.md5(repstr.encode(encoding='utf-8')).hexdigest()
+
+
+    def overwrite_status(self):
+        """Overwrite the status file, it's a JSON format dict"""
+        self.status['sim.t'] = self.sim.t
+        # formally, the params are redundant, as you can;t calculate the filename hash
+        #  without knowing them!
+        self.status['params'] = self.params
+        # Could add version, time, and host info here
+        with open(self.status_filename,'w') as json_file:
+            json.dump(self.status, json_file)
+        
+  
+    def init_rebound(self):
+        """Initialize the rebound simulation and rebx object for this model """
+        #in future, this will take an argument specifying to try to reload from disc
+        #self.get_model_status() will be called when restarts are enabled
+       
+        self.sim = rebound.Simulation()
+        self.sim.integrator = self.integrator
+        self.sim.collision = "line"
+        self.sim.heartbeat = self.heartbeat 
+
+        self.sim.automateSimulationArchive(self.simarchive_filename, walltime=self.snap_wall_interval, deletefile=True)
+        # status should be running or collided
+        self.status = {'status':'running'}
+        self.overwrite_status()
+
+        self.rebx = reboundx.Extras(self.sim)
+        mof = self.rebx.load_force("modify_orbits_resonance_relax")
+        mof.params['res_aspectratio0'] = self.aspectratio0
+        mof.params['res_sigma0'] = self.sigma0
+        mof.params['res_redge'] = self.redge
+        mof.params['res_deltaredge'] = self.deltaredge 
+        mof.params['res_alpha'] = self.alpha
+        mof.params['res_flaringindex'] = self.flaringindex
+        mof.params['res_ffudge'] = self.ffudge
+        mof.params['res_tdep'] = self.tdep
+        mof.params['res_deltatdep'] = self.deltatdep
+        self.rebx.add_force(mof)
+        self.mof = mof
+
+        self.sim.add(m=self.starmass, hash='star')
+
+        #init random number generator to get reproducible random phases
+        rng = random.Random(self.hash)
+        a = self.a0
+        for ip in range(1, self.nchain+1):
+            true_anomaly = rng.uniform(0, 2.0*np.pi)
+            print('adding a={:e}'.format(a))
+            pt = rebound.Particle(simulation=self.sim, primary=self.sim.particles[0],
+                                  m=self.pmass, a=a, f=true_anomaly)
+            pt.r = a*np.sqrt(pt.m/3.0)
+            self.sim.add(pt)
+            # the 0.05 is a spread. but maybe this could be a param
+            a *= (self.p_res / (self.p_res + self.q_res + 0.05))**(-2.0/3.0)
+        
+
+    def heartbeat(self, sim):
+       """Heartbeat callback from rebound integrate
+          sim is a pointer (see ctypes) not the actual object """
+       wall_now = time.time()
+       if (wall_now - self.lastheart > self.heart_print_interval):
+            self.lastheart = wall_now
+            print("time {} walltime {}".format(sim.contents.t, wall_now -self.wall_start), end='\r')
+
+
+    def mark_collision(self):
+        """Called when the driver gets a collision exception
+           Sets the model status, and records the collided particles 
+           Forces overwrite of the status file """
+        collided = []
+        for p in self.sim.particles:
+            if p.lastcollision == self.sim.t:
+                collided.append(p.index)
+        print('') # newline
+        print('Particles {} collided sim.t {}'.format(collided, self.sim.t))
+        self.status['status'] = 'stop collision'
+        self.status['collided'] = collided
+        self.overwrite_status()
+ 
+
+    def manual_snapshot(self):
+        """Call to save a manual snapshot, for example when driver hits walltime limit"""
+        self.sim.simulationarchive_snapshot(self.simarchive_filename)
+        self.overwrite_status()
--- chaincalc/testmodel.py
+++ chaincalc/testmodel.py
@@ -1,17 +1,54 @@
+import time
+import sys
+sys.path.append('/Users/colinm/vc/rebound')
+sys.path.append('../crbx')
 import chainmodel
+import rebound
+
+#this is sort of a dummy driver
+
+wall_start = time.time()
 
 p = {'aspectratio0':0.035,
      'sigma0':3.8e-4,
      'redge':0.1,
+     'a0':0.11,
      'deltaredge':0.001,
      'alpha':1.0,
      'flaringindex':2.0/7.0,
      'ffudge':1.0/100.0,
-     'tdep':1e5,
-     'deltatdep':1e3,
+     'tdep':1e1,
+     'deltatdep':1e1,
      'pmass':1e-5,
-     'nchain':9,
-     'p_res':4 }
-c = chainmodel.model(p)
+     'nchain':4,
+     'q_res':3,
+     'p_res':40 }
 
-print(c.hash)
+def runtest(p):
+    c = chainmodel.Model(p, wall_start)
+
+    c.init_rebound()
+
+    print(c.hash)
+    print(c.sim.status())
+
+    try:
+        c.sim.integrate(10000.0)
+    except rebound.Collision:
+        c.mark_collision()
+    else:
+        c.manual_snapshot()
+    print('')
+
+print('Running test with collision')
+runtest(p)
+
+print('Running test without collision')
+p['p_res'] = 5
+p['q_res'] = 1
+p['tdep'] = 1e5
+p['deltatdep'] = 1e3
+runtest(p)
+
+
+
