50a76f01a947628a58a83276a57da9fc75b696af
Colin McNally <colin@colinmcnally.ca>
2019-10-23 14:28:22 +0100

    update taudampmodel test

--- tests/testtaudampmodel.py
+++ tests/testtaudampmodel.py
@@ -20,20 +20,20 @@ p = {'tau_a':-1e5,
      'tau_inc':-1e3,
      'a0':0.11,
      'redge':0.1,
-     'deltaredge':1.0/100.0,
+     'deltaredge':0.1,
      'tdep':1e-10,
      'deltatdep':1e-10,
      'pmass':1e-5,
      'nchain':2,
-     'q_res':3,
-     'p_res':40,
+     'q_res':1,
+     'p_res':1,
      'seq':0,
      'collision':'line',
      'G':1.0,
      'starmass':1.0,
      'integrator':'WHFAST',
      'integrator_dt':1e-2*2*np.pi*0.1**1.5,
-     'snap_wall_interval':60*60,
+     'snap_wall_interval':15,
      'incscatter':np.pi/100.0,
      'aspread':0.05 }
 
@@ -47,20 +47,17 @@ def runtest(p):
 
     c.set_wall_start(wall_start)
     try:
-        c.sim.integrate(10000.0)
+        for i in range(0,50000):
+            c.sim.integrate((i+1)*100.0)
+            c.manual_snapshot()
     except rebound.Collision:
         c.mark_collision()
     else:
         c.manual_snapshot()
     print('')
 
-print('Running test with collision')
-runtest(p)
-
 print('Running test without collision')
-p['p_res'] = 5
-p['q_res'] = 1
-p['tdep'] = 1e5
+p['tdep'] = 1e8
 p['deltatdep'] = 1e3
 runtest(p)
 
